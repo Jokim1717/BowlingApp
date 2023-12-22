2a1aacb5001a6e1c0b03e8fb64aed79207af23fc
Joseph <josephkim1717@gmail.com>
2023-12-22 01:32:03 -0500

    started over with the frame implementation. - still need to center the frame

--- bowling-score-tracker/src/components/ScoreTracking/ScoreCard.js
+++ bowling-score-tracker/src/components/ScoreTracking/ScoreCard.js
@@ -1,23 +1,41 @@
-import './ScoreCard.css'; 
-import React, { useState } from 'react';
+import "./ScoreCard.css";
+import React, { useState } from "react";
 
 const ScoreCard = () => {
-  const frames = Array.from({ length: 10 }, (_, index) => index + 1);
+  const frames = Array.from({ length: 9 }, (_, index) => index + 1);
 
   return (
+    // <div>
+    //   <h2>Bowling Scoring Calculator</h2>
+    //   <div className="frame-container">
+    //     {frames.map((frameNumber) => (
+    //       <div key={frameNumber} className="frame-pair">
+    //         <p className="frame-number">Frame {frameNumber}</p>
+    //         <div className="frame-box">
+    //           <div className="score-box">
+    //             </div>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
     <div>
-      <h2>Bowling Scoring Calculator</h2>
-
-      <div className="frame-container">
+      <h2>Bowling Score Calculator</h2>
+      <div className="frameParent">
         {frames.map((frameNumber) => (
-          <div key={frameNumber} className="frame-pair">
-            <p className="frame-number">Frame {frameNumber}</p>
-            <div className="frame-box">
-              <div className="score-box">{/* Add score display here */}</div>
-              {/* You can add more styling or content as needed */}
-            </div>
+          <div key={frameNumber} className="frame">
+              <div className="throw1"></div>
+              <div className="throw2"></div>
+            <div className="throwResult"></div>
           </div>
         ))}
+        <div className="frame10">
+          <div className="frame10throw1"></div>
+          <div className="frame10throw2"></div>
+          <div className="frame10throw3"></div>
+          <div className="throwResult"></div>
+        </div>
+        <div className="totalScore"></div>
       </div>
     </div>
   );
