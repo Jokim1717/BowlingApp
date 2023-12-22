ff4f218d9cbdd9d75592a5d0a75b3bccb835ccdc
Joseph <josephkim1717@gmail.com>
2023-12-22 02:13:17 -0500

    working on handlingthrow()

--- bowling-score-tracker/src/components/ScoreTracking/ScoreCard.js
+++ bowling-score-tracker/src/components/ScoreTracking/ScoreCard.js
@@ -1,32 +1,51 @@
 import "./ScoreCard.css";
 import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 const ScoreCard = () => {
   const frames = Array.from({ length: 9 }, (_, index) => index + 1);
 
+  const [scores, setScores] = useState(Array(10).fill(null));
+  const [currentFrame, setCurrentFrame] = useState(1);
+  const [currentThrow, setCurrentThrow] = useState(1);
+
+  const handleThrow10 = (pins) => {
+
+  };
+
+  const handleThrow = (pins) => {
+    if (currentFrame === 10) {
+      handleThrow10(pins);
+
+    } else {
+      //if its the first throw
+      if (currentThrow === 1) {
+        if (pins === 10) {
+          //handle strike
+          setCurrentFrame(currentFrame + 1)
+          setCurrentThrow(1)
+          setScores
+        } else if (pins == 11) {
+          //handle spare
+          setCurrentFrame(currentFrame + 1)
+          setCurrentThrow(1)
+        } else {
+          //normal shot
+        }
+      }
+    }
+  };
+
+
   return (
-    // <div>
-    //   <h2>Bowling Scoring Calculator</h2>
-    //   <div className="frame-container">
-    //     {frames.map((frameNumber) => (
-    //       <div key={frameNumber} className="frame-pair">
-    //         <p className="frame-number">Frame {frameNumber}</p>
-    //         <div className="frame-box">
-    //           <div className="score-box">
-    //             </div>
-    //         </div>
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
     <div>
       <h2>Bowling Score Calculator</h2>
       <div className="frameParent">
         {frames.map((frameNumber) => (
           <div key={frameNumber} className="frame">
-              <div className="throw1"></div>
-              <div className="throw2"></div>
-            <div className="throwResult"></div>
+            <div className="throw1"></div>
+            <div className="throw2"></div>
+            <div className="throwResult">{scores[frameNumber]}</div>
           </div>
         ))}
         <div className="frame10">
@@ -36,6 +55,39 @@ const ScoreCard = () => {
           <div className="throwResult"></div>
         </div>
         <div className="totalScore"></div>
+      </div>
+      <div className="scoreParent">
+        <Button
+          className="scoreButton"
+          variant="secondary"
+          onClick={handleThrow(0)}
+        >
+          0
+        </Button>
+        {frames.map((frameNumber) => (
+          <Button
+            className="scoreButton"
+            variant="secondary"
+            key={frameNumber}
+            onClick={handleThrow(frameNumber)}
+          >
+            {frameNumber}
+          </Button>
+        ))}
+        <Button
+          className="scoreButton"
+          variant="secondary"
+          onClick={handleThrow(10)}
+        >
+          X
+        </Button>
+        <Button
+          className="scoreButton"
+          variant="secondary"
+          onClick={handleThrow(11)}
+        >
+          /
+        </Button>
       </div>
     </div>
   );
